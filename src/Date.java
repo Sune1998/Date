80bfb1a1dfdd7aa36ebc0a6bd185449e92b26714
sune <sune1309@edu.easj.dk>
2019-11-22 13:16:16 +0100

    added futer date

--- src/Date.java
+++ src/Date.java
@@ -1,5 +1,6 @@
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
+import java.util.Calendar;
 
 public class Date {
     public static void main(String[] args) {
@@ -7,5 +8,9 @@ DateTimeFormatter idag = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 LocalDateTime nu = LocalDateTime.now();
         System.out.println(idag.format(nu));
 
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, 160);
+        java.util.Date futerdatetime = calendar.getTime();
+        System.out.println(futerdatetime);
     }
 }
